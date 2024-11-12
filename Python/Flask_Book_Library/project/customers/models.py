863b5ee82e0e5ec953b4cb8bb660fff0154e52b7
kkehl <56897077+kkehl@users.noreply.github.com>
2024-11-12 20:46:46 +0100

    Update models.py
    Usunięto podatności XSS z zakładki Customers

--- Python/Flask_Book_Library/project/customers/models.py
+++ Python/Flask_Book_Library/project/customers/models.py
@@ -1,4 +1,5 @@
 from project import db, app
+import re
 
 
 # Customer model
@@ -9,9 +10,16 @@ class Customer(db.Model):
     city = db.Column(db.String(64))
     age = db.Column(db.Integer)
 
+    # Walidacja pola tekstowego za pomocą wyrażenia regularnego
+    def validate_text(self, text):
+        if not re.match("^[a-zA-Z0-9\s-]*$", text):
+            raise ValueError("Dozwolone są tylko małe i wielkie litery, cyfry, spacja oraz myślnik.")
+        return text
+
     def __init__(self, name, city, age):
-        self.name = name
-        self.city = city
+        # Walidacja każdego z pól tekstowych
+        self.name = self.validate_text(name)
+        self.city = self.validate_text(city)
         self.age = age
 
     def __repr__(self):
@@ -20,3 +28,4 @@ class Customer(db.Model):
 
 with app.app_context():
     db.create_all()
+
